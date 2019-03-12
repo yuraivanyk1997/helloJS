808532dc0e81731b8614b63eb967ad55e536d2f7
yuraivanyk1997 <ivanukyurii@gmail.com>
2019-03-13 00:29:06 +0200

    appended span

--- js/src.js
+++ js/src.js
@@ -3,3 +3,7 @@ const text = "Hello it is JS";
 const p = document.getElementById("par");
 
 p.innerHTML = text;
+
+const span = document.createElement("span");
+span.innerHTML = "      This is span created by JS";
+p.append(span);
